ef8feca60aad94fd3a6df8468ca2e72dc61c8748
YoshikageKira425 <eneshalili425@gmail.com>
2026-05-10 21:22:39 +0200

    From the menu you can enter your bag.

--- src/states/menuView.py
+++ src/states/menuView.py
@@ -11,18 +11,42 @@ class MenuView(arcade.View):
         
         self.overworld = overworld
         self.ui = MenuUi()
+        self.selectedIndex = 0
         
     def on_draw(self):
         self.clear()
         
         self.overworld.on_draw()
         
+        arcade.get_window().default_camera.use()
         self.ui.draw()
     
     def on_key_press(self, key, modifiers):
-        if self.isPressed(CONFIG.controls.cancel, key):
+        if self.isPressed(CONFIG.controls.interact, key):
+            self.action()
+        elif self.isPressed(CONFIG.controls.up, key):
+            self.selectedIndex -= 1
+            if self.selectedIndex == -1:
+                self.selectedIndex = len(self.ui.buttons) - 1
+                
+            self.ui.setYOfCursor(self.selectedIndex)
+        elif self.isPressed(CONFIG.controls.down, key):
+            self.selectedIndex += 1
+            if self.selectedIndex == len(self.ui.buttons):
+                self.selectedIndex = 0
+                
+            self.ui.setYOfCursor(self.selectedIndex)
+        elif self.isPressed(CONFIG.controls.cancel, key):
             self.window.show_view(self.overworld)
     
     def isPressed(self, configKey, key) -> bool:
         return getattr(arcade.key, configKey, None) == key
+    
+    def action(self):
+        if self.selectedIndex == 0:
+            pass
+        elif self.selectedIndex == 1:
+            self.window.show_view(BagView(self))
+        elif self.selectedIndex == 2:
+            pass
     
--- src/ui/menuUi.py
+++ src/ui/menuUi.py
@@ -9,7 +9,7 @@ class MenuUi:
         tilemap = arcade.load_tilemap("assets/ui/menuUiDesign.tmx")
         uiLayout = tilemap.get_tilemap_layer("ui")
         
-        self.buttons = []
+        self.buttons:list[arcade.gui.UILabel] = []
         
         for obj in uiLayout.tiled_objects:
             w = obj.size.width
@@ -40,6 +40,22 @@ class MenuUi:
                 
                 self.buttons.append(button)
                 self._manager.add(button)
+        
+        self._cursorLabel = arcade.Text(
+            text="▶",
+            x=0,y=0,
+            align="center",
+            color=arcade.color.BLACK,
+            font_name="Pokemon Emerald",
+            font_size=35
+        )
+        self.setYOfCursor(0)
             
     def draw(self):
         self._manager.draw()
+
+        self._cursorLabel.draw()
+        
+    def setYOfCursor(self, index:int):
+        self._cursorLabel.x = self.buttons[index].rect.left - 30
+        self._cursorLabel.y = self.buttons[index].rect.center_y - 13
