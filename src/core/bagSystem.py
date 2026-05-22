1a972d0967a440dda9d4b18ef53577f312bedc6a
YoshikageKira425 <eneshalili425@gmail.com>
2026-05-22 20:24:16 +0200

    Fixed a minor problem.

--- src/core/bagSystem.py
+++ src/core/bagSystem.py
@@ -6,18 +6,12 @@ class BagSystem:
     def __init__(self):
         self._items = saveManager.player.items
         self._pokeballs = saveManager.player.pokeballs
-<<<<<<< HEAD
     
     def useItem(self, itemIndex: int, pokemonId: str):
-=======
-
-    def useItem(self, itemIndex: int):
->>>>>>> 70b8707649d05718ddc8b9fe20909b301db59851
         if len(self._items) <= 0:
             return
 
         item = self._items[itemIndex]
-<<<<<<< HEAD
         
         if self._handleItemEffects(pokemonId.lower(), item.name):
             item.count -= 1
@@ -25,28 +19,14 @@ class BagSystem:
                 self._items.pop(itemIndex)
     
     def _handleItemEffects(self, pokemonId:str, itemId:str) -> bool:
-=======
-        item.count -= 1
-        if item.count <= 0:
-            self._items.pop(itemIndex)
-
-        self._handleItemEffects("combusken", item.name)
-
-    def _handleItemEffects(self, pokemonId: str, itemId: str):
->>>>>>> 70b8707649d05718ddc8b9fe20909b301db59851
         pokemon = saveManager.getPokemon(pokemonId)
         pokemonProfile = dataLoader.getPokemon(pokemonId)
         
         maxHp = ((2 * pokemonProfile.stats.hp * pokemon.level) // 100) + 5 + pokemon.level
         
         if not pokemon:
-<<<<<<< HEAD
             return False
         
-=======
-            return
-
->>>>>>> 70b8707649d05718ddc8b9fe20909b301db59851
         item = dataLoader.getItem(itemId)
 
         for effect in item.effects:
@@ -55,7 +35,6 @@ class BagSystem:
                     return False
                 
                 pokemon.hp += effect.amount
-<<<<<<< HEAD
                 if pokemon.hp > maxHp:
                     pokemon.hp = maxHp
                 
@@ -80,9 +59,6 @@ class BagSystem:
         
         return True
                 
-=======
-
->>>>>>> 70b8707649d05718ddc8b9fe20909b301db59851
     def getItems(self):
         return self._items
 
